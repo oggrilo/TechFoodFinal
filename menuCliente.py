90c394b3c55bbabfb9785626a6de5eec0d1de6a9
MURILO LEMOS MESQUITA <murilo.lmesquita@senacsp.edu.br>
2022-05-13 19:50:34 -0300

    first commit

---
+++ menuCliente.py
@@ -0,0 +1,35 @@
+import this
+import hamburgao
+import pizza
+import bauru
+
+this.opcao = 0
+def Menu ():
+    print('Escolha uma das opções abaixo:\n'+
+          '\n1. Hamburgão '+
+          '\n2. Pizza '+
+          '\n3. Bauru '+
+          '\n4. Voltar '+
+          '\n5. Fechar Programa')
+    this.opcao = int(input())
+
+def operacao():
+    #Mostrar o menu em tela
+    while this.opcao != 4:
+        Menu()
+        #realizar operações
+        if this.opcao == 1:
+            #operacao para 1.
+            hamburgao.operacao()
+        elif this.opcao == 2:
+            #operacao para 2.
+            pizza.operacao()
+        elif this.opcao == 3:
+            # operacao para 3.
+            bauru.operacao()
+        elif this.opcao == 4:
+            login.operacao()
+        elif this.opcao == 5:
+            print('Fechando... agradecemos sua presença aqui!')
+        else:
+            print('Opção escolhida inválida! Tente novamente com as opções fornecidas.')
---
+++ pizza.py
@@ -0,0 +1,75 @@
+import this
+import menuCliente
+import conexao
+import mysql.connector
+
+this.opcao = 0
+
+db_connection = conexao.conectar() #Abrindo a conexão com o banco de dados
+con = db_connection.cursor()
+
+def PrecoLanche():
+    sql = "select * from lanche where nomeLanche = '{Pizza}'"
+    con.execute(sql)
+
+    for (nomeLanche, descricaoLanche, valorLanche, quantidadeLanche) in con:
+        print(valorLanche)
+    print('\n')
+
+def Preco ():
+    print('O preço do Pizza é: R$' + str(PrecoLanche()) + ',00.')
+
+def QuantidadeLanche():
+    sql = "select * from lanche where quantidadeLanche = '{Pizza}'"
+    con.execute(sql)
+
+    for (nomeLanche, descricaoLanche, valorLanche, quantidadeLanche) in con:
+        print(quantidadeLanche)
+    print('\n')
+
+def Quantidade():
+    print('A quantidade disponível desse lanche é: ' + str(QuantidadeLanche()) + '.')
+
+def CalculoQuant():
+    this.quantidade - QuantidadeLanche()
+
+def Selecao (nomeLanche, quantidadeLanche):
+    print('Digite a quantidade que deseja comprar: ')
+    this.quantidade = int(input())
+    while (this.quantidade < 0) or (this.quantidade > QuantidadeLanche()):
+        if (this.quantidade < 0) or (this.quantidade > QuantidadeLanche()):
+            print('Informe uma quantidade acessível à disponível, por favor!')
+            this.quantidade = int(input())
+    sql = "update lanche set quantidadeLanche = '{}' where nomeLanche = '{Pizza}'".format(nomeLanche,quantidadeLanche)
+    con.execute(sql)
+    db_connection.commit()
+
+def Calculo():
+    return this.quantidade * QuantidadeLanche()
+
+def Compra ():
+    Preco()
+    Quantidade()
+    Selecao()
+    print('---------------- O valor total da compra é: '+ str(Calculo()) +',00. ----------------\n')
+def Menu ():
+    Compra()
+    print('Deseja:'
+          '\n1. Realizar Compra' +
+          '\n2. Cancelar Compra')
+    this.opcao = int(input())
+
+def operacao():
+        # Mostrar o menu em tela
+        while this.opcao != 2:
+            Menu()
+            #realizar operações
+            if this.opcao == 1:
+                #operacao para 1.
+                Compra()
+                print('O código do PIX é 12345678912, acesse https://www.picpay.com/site para efetuar pagamento.')
+            elif this.opcao == 2:
+                #opereção para 2.
+                menuCliente.operacao()
+            else:
+                print('Opção escolhida inválida! Tente novamente com as opções fornecidas.')
